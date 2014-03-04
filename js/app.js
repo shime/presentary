64ae4a7e955f27f08aaecdb7d156a7b4fa4eee77
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-03-04 11:13:40 +0100

    start showing slides

--- js/app.js
+++ js/app.js
@@ -1,6 +1,73 @@
 App = Ember.Application.create({
+  LOG_TRANSITIONS: true
 });
 
 App.ApplicationView = Em.View.extend({
   classNames: ["main"]
+});
+
+App.Router.map(function(){
+  this.resource("slides", function(){
+    this.route("show", {path: ":id"});
+  });
+});
+
+App.Router.reopen({
+  enableLogging: true
+});
+
+App.ApplicationRoute = Em.Route.extend({
+  redirect: function(){
+    this.transitionTo("slides");
+  }
+})
+
+App.Slide = Ember.Object.extend({
+});
+
+App.Slide.reopenClass({
+  findAll: function(){
+    var self = this;
+    return Em.Deferred.promise(function(p){
+      if (self.loadedSlides === true) {
+        p.resolve(self.loadedSlides);
+      } else {
+        p.resolve(Em.$.get("/slides.json").then(function(response){
+          var slides = Em.A();
+          response.forEach(function(child){
+            var slide = App.Slide.create({
+              title: child.title,
+              id: child.id
+            });
+            slides.pushObject(slide);
+          });
+          self.slides = slides;
+          self.loadedSlides = true;
+          return slides;
+        }));
+      }
+    });
+  },
+
+  findById: function(id){
+    return this.findAll().then(function(slides){
+      return slides.findProperty('id', id);
+    });
+  }
+
+});
+
+App.SlidesRoute = Em.Route.extend({
+  model: function(){
+    return App.Slide.findAll();
+  },
+  setupController: function(controller, slides) {
+    this.transitionTo("slides.show", slides.get("firstObject"));
+  }
+});
+
+App.SlidesShowRoute = Em.Route.extend({
+  model: function(params){
+    return App.Slide.findById(params.id);
+  }
 });
